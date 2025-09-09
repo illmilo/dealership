c3dcbea227156b64498a362e93a1d6f71c5e903b
illmilo <illfqm@gmail.com>
2025-09-09 23:44:39 +0300

    Testing things without build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 echo "BUILD START"
 
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
-# python3 manage.py makemigrations
-# python3 manage.py migrate
-
-# python3 manage.py collectstatic --noinput --clear
-
-# mkdir -p staticfiles/media/
-# cp -r media/* staticfiles/media/
+python3 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
