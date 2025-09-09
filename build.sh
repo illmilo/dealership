acfe5bb4644471153e026cc0e2ba3e1f89d57245
illmilo <illfqm@gmail.com>
2025-09-09 23:36:09 +0300

    Testing without collectstatic

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@ echo "BUILD START"
 
 python -m pip install -r requirements.txt
 
-python3 manage.py makemigrations
-python3 manage.py migrate
+# python3 manage.py makemigrations
+# python3 manage.py migrate
 
-python3 manage.py collectstatic --noinput --clear
+# python3 manage.py collectstatic --noinput --clear
 
-mkdir -p staticfiles/media/
-cp -r media/* staticfiles/media/
+# mkdir -p staticfiles/media/
+# cp -r media/* staticfiles/media/
 
 echo "BUILD END"
