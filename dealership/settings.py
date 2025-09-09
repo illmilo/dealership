5101d1b789a9ce9afa8f4b3a325deb831ed995c6
illmilo <illfqm@gmail.com>
2025-09-09 23:27:12 +0300

    Testing staticfiles without vercel --prod

--- dealership/settings.py
+++ dealership/settings.py
@@ -115,7 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
